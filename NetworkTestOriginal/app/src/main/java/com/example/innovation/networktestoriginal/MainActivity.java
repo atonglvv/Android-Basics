3ce8d8553be82d1ce1d85f9262b4275a063c0f5e
lgct <lt1550919167@gmail.com>
2017-10-28 14:43:59 +0800

    initial commit
    网络操作，获取www.qq.com数据

---
+++ NetworkTestOriginal/app/src/main/java/com/example/innovation/networktestoriginal/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.innovation.networktestoriginal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView responseText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView)findViewById(R.id.tv_response);
+
+        sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendRequestWithHttpURLConnection();
+            }
+        });
+
+    }
+
+    private void sendRequestWithHttpURLConnection(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("http://www.qq.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    // 设置连接主机超时 ms
+                    connection.setConnectTimeout(8000);
+                    //设置从主机读取数据超时
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    //对获取到的输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseText.setText(response);
+            }
+        });
+    }
+
+}
