c7dae94fd5bd5a062d99776549198c5b7e40402b
lgct <lt1550919167@gmail.com>
2017-10-31 19:10:56 +0800

    Notification
    简单的Notification
    只有小标题、标题、内容。。。

---
+++ Notification/app/src/main/java/com/example/innovation/notification/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.innovation.notification;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+/*
+* 简单的Notification
+* @author Ubi.Innovation
+* Learning In   http://www.cnblogs.com/travellife/p/Android-Notification-xiang-jie.html
+* @since 1.0*/
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.button)
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+    @OnClick(R.id.button)
+    public void OnClick(){
+        sendNotification();
+    }
+
+    private void sendNotification() {
+        //获取NotificationManager实例
+        NotificationManager notifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //实例化NotificationCompat.Builde并设置相关属性
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                //设置小图标
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                //设置通知标题
+                .setContentTitle("最简单的Notification")
+                //设置通知内容
+                .setContentText("只有小图标、标题、内容");
+        //设置通知时间，默认为系统发出通知的时间，通常不用设置
+        //.setWhen(System.currentTimeMillis());
+        //通过builder.build()方法生成Notification对象,并发送通知,id=1
+        notifyManager.notify(1, builder.build());
+    }
+
+}
