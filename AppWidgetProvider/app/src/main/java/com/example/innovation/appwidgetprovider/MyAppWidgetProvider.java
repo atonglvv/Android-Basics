a403465f2ea01462871d3d6807e53ddc1b7962c7
lgct <lt1550919167@gmail.com>
2017-11-03 17:25:58 +0800

    AppWidgetProvider initial commit

---
+++ AppWidgetProvider/app/src/main/java/com/example/innovation/appwidgetprovider/MyAppWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.example.innovation.appwidgetprovider;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by lt155 on 2017/11/3.
+ */
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
+
+    public MyAppWidgetProvider() {
+        super();
+    }
+
+
+}
